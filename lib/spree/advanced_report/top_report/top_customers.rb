060629c67a71786809984a79d3c7dc9cd64d2e37
Greg Reinacker <gregr@rassoc.com>
2013-02-10 16:44:35 -0700

    use limit instead of hard-coded 4
    Conflicts:
    	lib/spree/advanced_report/top_report/top_customers.rb

--- lib/spree/advanced_report/top_report/top_customers.rb
+++ lib/spree/advanced_report/top_report/top_customers.rb
@@ -23,8 +23,8 @@ class Spree::AdvancedReport::TopReport::TopCustomers < Spree::AdvancedReport::To
     end
 
     self.ruportdata = Table(I18n.t("adv_report.top_report.top_customers.table"))
-    data.inject({}) { |h, (k, v) | h[k] = v[:revenue]; h }.sort { |a, b| a[1] <=> b [1] }.reverse[0..4].each do |k, v|
-      ruportdata << { "email" => data[k][:email], I18n.t("adv_report.units") => data[k][:units], I18n.t("adv_report.revenue") => data[k][:revenue] } 
+    data.inject({}) { |h, (k, v) | h[k] = v[:revenue]; h }.sort { |a, b| a[1] <=> b [1] }.reverse[0..limit].each do |k, v|
+      ruportdata << { "email" => data[k][:email], "Units" => data[k][:units], "Revenue" => data[k][:revenue] } 
     end
     ruportdata.replace_column(I18n.t("adv_report.revenue")) { |r| "$%0.2f" % r.send(I18n.t("adv_report.revenue")) }
     ruportdata.rename_column("email", I18n.t("adv_report.top_report.top_customers.customer_email"))
